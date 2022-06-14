f5d5ce81a420a99001ded7d393ded8e0fe9cdfd3
Celso Yamashita <ctyamashita@hotmail.com>
2022-06-14 18:44:43 +0900

    workshop first commit

--- workshop/src/index.jsx
+++ workshop/src/index.jsx
@@ -1,1 +1,23 @@
 import '../assets/stylesheets/application.scss';
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// Functional component
+const Hello = (props) => {
+  const { name, age } = props;
+  return <h1>Hello { name }, { age } years old.</h1>;
+};
+
+// Class component
+
+const container = document.querySelector("#root");
+
+console.log('connected');
+
+if (container) {
+  // create root
+  const root = ReactDOM.createRoot(container);
+  // render something in the root
+  root.render(<Hello name="Luis" />); // calling a component (Hello)
+}
